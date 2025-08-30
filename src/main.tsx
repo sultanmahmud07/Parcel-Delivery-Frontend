f6de27469338461ae608fec7fea28074d79e87d6
Md Shimul Molla <shimuliithost@gmail.com>
2025-08-30 17:44:04 +0600

    Project setup and config

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { router } from "./routes/index.tsx";
+import { ThemeProvider } from "./providers/theme.provider.tsx";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./redux/store.ts";
+import { Toaster } from "./components/ui/sonner.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
+    </ReduxProvider>
+  </React.StrictMode>
+);
---
+++ src/pages/User/Bookings.tsx
@@ -0,0 +1,7 @@
+export default function Bookings() {
+  return (
+    <div>
+      <h1> This is Bookings component </h1>
+    </div>
+  );
+}
